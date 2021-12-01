72f3102dc4ee2034d89d62240c2e9a5a40d68c6a
krish-evans <krishna0910@hotmail.com>
2021-12-01 22:55:03 +0530

    unba

--- 29-Nov/kbcfn.js
+++ 29-Nov/kbcfn.js
@@ -1,39 +1,45 @@
-var bfalse = function(ans){
-    if(ans== "B"||"b"){
-        console.log("We regret to inform you that you opted the wrong answer\n You are disqualified");
-        alert("Wrong answer \n You are disqualified");
-        break;
+var evaluation = function (e){
+    if(e=="A"||"a"){
+        console.log("Correct Answer");
+    }
+    else{
+        console.log("Wrong Answer");
+        brk();
     }
 }
 
-var afalse = function(ansr){
-    if(ansr == "A"||"a"){
-        console.log("We regret to inform you that you opted the wrong answer\n You are disqualified");
-        alert("Wrong answer \n You are disqualified");
-        break; 
+var btrue = function (b){
+    if(b== "B"||"b"){
+        console.log("Correct Answer");
+    }
+    else{
+        console.log("Wrong answer");
+        brk();
     }
 }
+
+
 
 var question = function(ques){
     if(ques==1){
         var a = window.prompt("Which represents the balloon\n (A)🎈\n (B)🧨");
-        bfalse(a);
+        evaluation(a);
     }
     if(ques==2){
         var a = window.prompt("Which represents the pig \n (A)🐄 \n (B)🐖");
-        afalse(a);
+        btrue(a);
     }
     if(ques==3){
-        var a = window.prompt("Which represents the balloon\n (A)🎈\n (B)🧨");
-        bfalse(a);
+        var a = window.prompt("Which represents the cat\n (A)🐈 \n (B)🐖");
+        evaluation(a);
     }
     if(ques==4){
-        var a = window.prompt("Which represents the balloon\n (A)🎈\n (B)🧨");
-        bfalse(a);
+        var a = window.prompt("which represents the dog \n (A)🐩 (B)🐄");
+        evaluation(a);
     }
     if(ques==5){
-        var a = window.prompt("Which represents the balloon\n (A)🎈\n (B)🧨");
-        bfalse(a);
+        var a = window.prompt("which represents the Crossaint \n (A)🥐 (B)🍟");
+        evaluation(a);
     }
     if(ques==6){
         var a = window.prompt("Congratulations! you've sucessfully completed current question sequence");
@@ -43,8 +49,15 @@ var question = function(ques){
 var nm = window.prompt("Provide your name to proceed further");
 
 var i = " ";
+
+// var brk = " ";
+var brk = function(brek){
+    break ;
+}
+
+loop1:
 for(i=1; i<=6; i++){
-    i= ques;
+    // i= ques;
     question(i); 
 }
 
