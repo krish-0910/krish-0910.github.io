835646c2e616cd003852685872eb0edc867554f4
krish-evans <krishna0910@hotmail.com>
2021-12-19 12:35:14 +0530

    update

--- Test-5/script.js
+++ Test-5/script.js
@@ -3,8 +3,9 @@ async function getdata() {
     var data= await res.json();
     
     document.getElementById('view').src=data.preview[0];
+    document.getElementById("url").href = data.url;
     document.getElementById('url').innerText=data.url;
     console.log(data.url);
+    
 }
 
-    
