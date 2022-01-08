923b2db94ae8b3f01cde369b5048018951d27dac
krish-evans <krishna0910@hotmail.com>
2022-01-08 13:11:15 +0530

    update

--- Advanced Front End/Week-2/Assignment-1/prototype.js
+++ Advanced Front End/Week-2/Assignment-1/prototype.js
@@ -1,16 +1,60 @@
-Array.prototype.prime = function (a){
+// Array.prototype.prime = function () {
+//     let i;
+//     let s;
+//     this.forEach(e=>{
+//         for(i=1;i<e;i++){
+//           s= e%i;
+//             if(s==0){
+//                 return false;
+//             }  
+//              else{
+//                  return true;
+//}
+//         }
+//     })
+// 
 
-    let i;
-    a.array.forEach(element = function() {
-        for (i=1;i<element;i++){
-            if(element%i==0){
-                return false;
-            }
-            else{
-                return true;
-            }
+// }
+
+Array.prototype.prime = function(){
+    for(var i =0; i<this.length; i++){
+        if(isprime(this[i])){
+            document.write("TRUE - The given set of elements contain a prime number"+"<br><br>");
+            return true;
         }
-    });
-
+    }
+    document.write("FALSE - The given set of elements do not contain prime number"+"<br><br>");
+    return false;
 }
+
+const isprime = (x) => {
+    if(x<=2){
+        return true;
+    }
+    for (var i =2 ; i<x; i++){
+        if(x%i==0) {
+           
+            return false;
+           
+        }
+    }
+    
+    return true;
+    
+}
+
+const test = (m) => {
+    return console.log(m.prime());
+}
+
+let arr2 = [12, 7, 22];
+
+test(arr2);
+
+
+
+let arr = [10, 12, 21];
+
+console.log(arr.prime());
+
 
