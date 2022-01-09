6992d29ad76669526362c1eacf34274499708cb5
krish-evans <krishna0910@hotmail.com>
2022-01-10 02:37:31 +0530

    UPDATE

--- Advanced Front End/Week-2/Assignment-2/Screen_Lock/script.js
+++ Advanced Front End/Week-2/Assignment-2/Screen_Lock/script.js
@@ -1,31 +1,87 @@
-const inputs = document.querySelectorAll("input");
-const codeBlock = document.getElementById("code-block");
-const code = document.getElementById("code");
-const form = document.querySelector("form");
+class PinLogin {
+  constructor ({el, loginEndpoint, redirectTo, maxNumbers = Infinity}) {
+      this.el = {
+          main: el,
+          numPad: el.querySelector(".pin-login__numpad"),
+          textDisplay: el.querySelector(".pin-login__text")
+      };
 
-inputs.forEach((input, key) => {
-  if (key !== 0) {
-    input.addEventListener("click", function () {
-      inputs[0].focus();
-    });
+      this.loginEndpoint = loginEndpoint;
+      this.redirectTo = redirectTo;
+      this.maxNumbers = maxNumbers;
+      this.value = "";
+
+      this._generatePad();
   }
-  input.addEventListener("keyup", function () {
-    if (input.value) {
-      if (key === 3) {
-        // Last one tadaa
-        const userCode = [...inputs].map((input) => input.value).join("");
-        codeBlock.classList.remove("hidden");
-        code.innerText = userCode;
-      } else {
-        inputs[key + 1].focus();
+
+  _generatePad() {
+      const padLayout = [
+          "1", "2", "3",
+          "4", "5", "6",
+          "7", "8", "9",
+          "backspace", "0", "done"
+      ];
+
+      padLayout.forEach(key => {
+          const insertBreak = key.search(/[369]/) !== -1;
+          const keyEl = document.createElement("div");
+
+          keyEl.classList.add("pin-login__key");
+          keyEl.classList.toggle("material-icons", isNaN(key));
+          keyEl.textContent = key;
+          keyEl.addEventListener("click", () => { this._handleKeyPress(key) });
+          this.el.numPad.appendChild(keyEl);
+
+          if (insertBreak) {
+              this.el.numPad.appendChild(document.createElement("br"));
+          }
+      });
+  }
+
+  _handleKeyPress(key) {
+      switch (key) {
+          case "backspace":
+              this.value = this.value.substring(0, this.value.length - 1);
+              break;
+          case "done":
+              this._attemptLogin();
+              break;
+          default:
+              if (this.value.length < this.maxNumbers && !isNaN(key)) {
+                  this.value += key;
+              }
+              break;
       }
-    }
-  });
+
+      this._updateValueText();
+  }
+
+  _updateValueText() {
+      this.el.textDisplay.value = "_".repeat(this.value.length);
+      this.el.textDisplay.classList.remove("pin-login__text--error");
+  }
+
+  _attemptLogin() {
+      if (this.value.length > 0) {
+          fetch(this.loginEndpoint, {
+              method: "post",
+              headers: {
+                  "Content-Type": "application/x-www-form-urlencoded"
+              },
+              body: `pincode=${this.value}`
+          }).then(response => {
+              if (response.status === 200) {
+                  window.location.href = this.redirectTo;
+              } else {
+                  this.el.textDisplay.classList.add("pin-login__text--error");
+              }
+          })
+      }
+  }
+}
+
+new PinLogin({
+  el: document.getElementById("mainPinLogin"),
+  loginEndpoint: "login.php",
+  redirectTo: "dashboard.html"
 });
-
-const reset = () => {
-  form.reset();
-  codeBlock.classList.add("hidden");
-  code.innerText = "";
-};
-
