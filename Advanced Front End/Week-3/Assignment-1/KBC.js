905500b39f2b92e0ff22789b37e29867b1d6cf57
krish-evans <krishna0910@hotmail.com>
2022-01-18 19:44:59 +0530

    update

--- Advanced Front End/Week-3/Assignment-1/KBC.js
+++ Advanced Front End/Week-3/Assignment-1/KBC.js
@@ -123,11 +123,11 @@ function scoreRender(){
     
     const scorePerCent = Math.round(100 * score/questions.length);
     
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
-              "img/1.png";
+    let img = (scorePerCent >= 80) ? "" :
+              (scorePerCent >= 60) ? "" :
+              (scorePerCent >= 40) ? "" :
+              (scorePerCent >= 20) ? "" :
+              "";
     
     scoreDiv.innerHTML = "<img src="+ img +">";
     scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
